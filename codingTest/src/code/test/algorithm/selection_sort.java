0cb20b023221a63ff8e87ff10851a79e63e5471b
januarry22 <tls2330@naver.com>
2022-06-22 22:17:25 +0900

    선택정렬

---
+++ codingTest/src/code/test/algorithm/selection_sort.java
@@ -0,0 +1,36 @@
+package code.test.algorithm;
+
+/*
+* JAVA 선택 정렬 selection sort
+* */
+public class selection_sort {
+    public static void main(String[] args){
+        int[] selection = {3, 5, 10, 1, 8, 7, 2, 4};
+
+
+        for(int i = 0; i<selection.length-1;i++){
+            int lowest = i;
+            for(int j = i+1 ; j < selection.length; j++) {
+                /*
+                 * 최소값 찾을 때 까지 루프
+                 */
+                if(selection[lowest] > selection [j]) {
+                     lowest = j;
+                }
+            }
+            swap(selection, lowest, i);
+        }
+
+        for (int n : selection){
+            System.out.print(n+"\n");
+        }
+    }
+
+    public static void swap(int[] nonebubble, int prev, int next){
+
+        int temp = nonebubble[prev];
+        nonebubble[prev] = nonebubble[next];
+        nonebubble[next] = temp;
+
+    }
+}
