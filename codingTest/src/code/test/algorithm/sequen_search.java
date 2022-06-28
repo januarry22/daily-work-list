b1b6776a1cf1cb22cf63d9a3d0e7c65df98e9fbc
januarry22 <tls2330@naver.com>
2022-06-28 15:50:40 +0900

    이진 탐색, 순차 탐색

---
+++ codingTest/src/code/test/algorithm/sequen_search.java
@@ -0,0 +1,26 @@
+package code.test.algorithm;
+
+import java.util.Arrays;
+
+/*
+* JAVA 순차 탐색
+*
+* */
+public class sequen_search {
+    public static int[] arr = {5, 2, 3, 1, 9, 11, 15, 16, 33, 23};
+    public static void main(String[] args){
+
+        System.out.print("index :"+  sequenSearch(15));
+        System.out.print("not in index :"+  sequenSearch(18));
+    }
+
+    static int sequenSearch(int key) {
+
+        for(int i = 0; i<arr.length; i++){
+            if (key == arr[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
