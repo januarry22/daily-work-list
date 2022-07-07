c2dc327fdb755553fabd6706e6a6c084eb6242ae
januarry22 <tls2330@naver.com>
2022-07-07 20:03:42 +0900

    풀이

---
+++ codingTest/src/code/test/baekjoon/problem17389.java
@@ -0,0 +1,38 @@
+package code.test.baekjoon;
+
+/*
+ * 백준 - 보너스 점수
+ * */
+public class problem17389 {
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//        int[] wait = new int[N];
+//        for(int i = 0; i < N; i++) {
+//            wait[i] = sc.nextInt();
+//        }
+        int N = 8;
+        String str = "XOOOXOOX";
+
+
+        int sum=0;
+        int bonus =0;
+        int result = 0;
+        int score = 0;
+        for(int i = 0; i<str.length(); i++){
+               if(str.charAt(i)=='O'){
+                   score = (i+1);
+                   result = score + bonus;
+                   bonus++;
+               }else{
+                   bonus = 0;
+                   result = 0;
+               }
+            sum+=result;
+        }
+
+        System.out.println(sum);
+    }
+
+}
