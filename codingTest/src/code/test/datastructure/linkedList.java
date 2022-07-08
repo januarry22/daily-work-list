c3988b429a8b275534d8090da167f80affc8f8f2
januarry22 <tls2330@naver.com>
2022-07-08 19:49:01 +0900

    linked list

---
+++ codingTest/src/code/test/datastructure/linkedList.java
@@ -0,0 +1,22 @@
+package code.test.datastructure;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/*
+* JAVA 배열
+* */
+public class linkedList {
+    public static void main(String[] args){
+        // 타입 미설정시 object 로 세팅됨
+        LinkedList<String> linkedList = new LinkedList();
+
+        // 리스트 추가
+        linkedList.add("item1");
+        linkedList.add("item2");
+        linkedList.add("item3");
+
+
+    }
+
+}
