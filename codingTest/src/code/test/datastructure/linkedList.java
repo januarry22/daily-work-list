7a34288913d0d3d3a30a36c05a6dbd914da422d1
januarry22 <tls2330@naver.com>
2022-07-09 16:48:47 +0900

    linked list

--- codingTest/src/code/test/datastructure/linkedList.java
+++ codingTest/src/code/test/datastructure/linkedList.java
@@ -1,5 +1,6 @@
 package code.test.datastructure;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Stack;
 
@@ -17,6 +18,28 @@ public class linkedList {
         linkedList.add("item3");
 
 
+
+        linkedList.addFirst("first");
+        // 첫번째 인덱스에 추가
+        linkedList.addLast("last");
+        // 마지막 인덱스에 추가
+
+        // for 문으로 출
+        for(String a :linkedList){
+            System.out.println(a);
+        }
+
+        // Iterator 클래스로도 출력 가능
+        Iterator<String> iter = linkedList.iterator(); //Iterator 선언
+        while(iter.hasNext()){//다음값이 있는지 체크
+            System.out.println(iter.next()); //값 출력
+        }
+
+
+        linkedList.remove(1);
+        // 삭제
+        linkedList.clear();
+        // 모든 값 제거
     }
 
 }
