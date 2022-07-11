f78fecaa4a26fb9fa85fcefd3cb6fadd279fe214
januarry22 <tls2330@naver.com>
2022-07-11 16:06:04 +0900

    merge sort

--- codingTest/src/code/test/algorithm/mergesort.java
+++ codingTest/src/code/test/algorithm/mergesort.java
@@ -5,127 +5,52 @@ import java.util.Arrays;
 import java.util.List;
 
 /*
-* JAVA 병합 정렬 merge sort
+ * JAVA 병합 정렬 merge sort
  * @param arr		정렬할 배열
  * @param left	배열의 시작점
  * @param mid	배열의 중간점
  * @param right	배열의 끝 점
-* */
+ * */
 public class mergesort {
-    private static int[] SORTED;
-    public static void main(String[] args){
 
-        int[] arr = {5, 2, 3, 1};
+    public static void main(String[] args) {
 
+        int[] arr = {5, 2, 3, 1, 7, 4, 6, 9};
 
-        SORTED = new int[arr.length];
-        merge_split(arr, 0, arr.length-1);
-        SORTED = null;
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(merge(arr)));
     }
 
-    public static void merge_split(int arr[], int left, int right){
-        if(left == right){
-            return;
+    public static int[] merge(int arr[]) {
+        if (arr.length<2){
+            return arr;
         }
-        int mid = (left + right)/2;
+        int mid = (arr.length) / 2;
 
-        merge_split(arr, left, mid);		// 절반 중 왼쪽 부분리스트(left ~ mid)
-        merge_split(arr, mid + 1, right);	// 절반 중 오른쪽 부분리스트(mid+1 ~ right)
+        int[] left_arr = merge(Arrays.copyOfRange(arr, 0, mid));
+        int[] right_arr = merge(Arrays.copyOfRange(arr, mid, arr.length));
 
-        merge_sort(arr, left, mid, right);
+        int[] SORTED = new int[arr.length];
+        int left_p=0, right_p=0, idx=0;
+
+        while(left_p < left_arr.length && right_p <right_arr.length){
+            if(left_arr[left_p] < right_arr[right_p]){
+                SORTED[idx++] = left_arr[left_p++];
+            }else{
+                SORTED[idx++] = right_arr[right_p++];
+            }
+        }
+
+
+        while(left_p < left_arr.length){
+            SORTED[idx++] = left_arr[left_p++];
+        }
+
+        while (right_p < right_arr.length){
+            SORTED[idx++] = right_arr[right_p++];
+        }
+
+        return SORTED;
     }
 
-
-    public static void merge_sort(int arr[],int left,int mid, int right){
-    //   int[] SORTED = new int[arr.length];
-
-        int left_point = left;
-        int right_point = mid+1;
-        int idx=left;              // 안정정렬이기 때문에 index값 설정
-
-        while(left_point <= mid && right_point <= right) {
-            /*
-             *  왼쪽 부분리스트 l번째 원소가 오른쪽 부분리스트 r번째 원소보다 작거나 같을 경우
-             *  왼쪽의 l번째 원소를 새 배열에 넣고 l과 idx를 1 증가시킨다.
-             */
-            if(arr[left_point] <= arr[right_point]) {
-                SORTED[idx] = arr[left_point];
-                idx++;
-                left_point++;
-            }
-            /*
-             *  오른쪽 부분리스트 r번째 원소가 왼쪽 부분리스트 l번째 원소보다 작거나 같을 경우
-             *  오른쪽의 r번째 원소를 새 배열에 넣고 r과 idx를 1 증가시킨다.
-             */
-            else {
-                SORTED[idx] = arr[right_point];
-                idx++;
-                right_point++;
-            }
-        }
-
-        /*
-         * 왼쪽 부분리스트가 먼저 모두 새 배열에 채워졌을 경우 (l > mid)
-         * = 오른쪽 부분리스트 원소가 아직 남아있을 경우
-         * 오른쪽 부분리스트의 나머지 원소들을 새 배열에 채워준다.
-         */
-        if(left_point > mid) {
-            while(right_point <= right) {
-                SORTED[idx] = arr[right_point];
-                idx++;
-                right_point++;
-            }
-        }
-
-        /*
-         * 오른쪽 부분리스트가 먼저 모두 새 배열에 채워졌을 경우 (r > right)
-         * = 왼쪽 부분리스트 원소가 아직 남아있을 경우
-         * 왼쪽 부분리스트의 나머지 원소들을 새 배열에 채워준다.
-         */
-        else {
-            while(left_point <= mid) {
-                SORTED[idx] = arr[left_point];
-                idx++;
-                left_point++;
-            }
-        }
-
-
-//        //TODO : left, right 모두 있을떄
-//        while (left_point < mid && right_point < right){
-//            if (arr[left_point] > arr[right_point]){
-//                SORTED[idx] = arr[right_point];
-//                idx++;
-//                right_point++;
-//            }else{
-//                SORTED[idx] = arr[left_point];
-//                idx++;
-//                left_point++;
-//            }
-//        }
-//
-//        //TODO : left 만 있을때
-//        while(left_point >= mid){
-//            SORTED[idx] = arr[left_point];
-//            idx++;
-//            left_point++;
-//        }
-//
-//        //TODO : right 만 있을때
-//        while(right_point >= right){
-//            SORTED[idx] = arr[right_point];
-//            idx++;
-//            right_point++;
-//        }
-
-        for(int a : SORTED){
-            System.out.print(a+"\n");
-        }
-
-
-        for(int i = left; i <= right; i++) {
-            arr[i] = SORTED[i];
-        }
-     //   return SORTED;
-    }
 }
--- daily-study/algorithm/병합 정렬 (Merge Sort).md
+++ daily-study/algorithm/병합 정렬 (Merge Sort).md
@@ -3,9 +3,7 @@
 # 1. 병합정렬 (Merge Sort)?
 
 - 재귀용법을 활용한 정렬 알고리즘
-    1. 리스트를 절반으로 잘라 비슷한 크기의 두 부분 리스트로 나눈다.
-    2. 각 부분 리스트를 재귀적으로 합병 정렬을 이용해 정렬한다.
-    3. 두 부분 리스트를 다시 하나의정렬된 리스트로 합병한다.
+    
     
     ![https://blog.kakaocdn.net/dn/GD4LL/btqJWSDyKHS/2RNiqbImdYuC5QEUzDT0zk/img.gif](https://blog.kakaocdn.net/dn/GD4LL/btqJWSDyKHS/2RNiqbImdYuC5QEUzDT0zk/img.gif)
     
@@ -13,13 +11,55 @@
     
     [https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F07jQt%2Fbtq1lao22zT%2FKkr0QfF1VGxi3bfGYp2r61%2Fimg.png](https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F07jQt%2Fbtq1lao22zT%2FKkr0QfF1VGxi3bfGYp2r61%2Fimg.png)
     
-    **1. 주어진 리스트를 절반으로 분할하여 부분리스트로 나눈다. (Divide : 분할)**
+    1. **리스트를 절반으로 잘라 비슷한 크기의 두 부분 리스트로 나눈다.**
+    2. **부분 리스트의 길이가 1이 아니라면 1번 과정을 반복한다.**
+    3. **인접한 두 부분 리스트를 다시 하나의정렬된 리스트로 합병한다.**
     
-    **2. 해당 부분리스트의 길이가 1이 아니라면 1번 과정을 되풀이한다.**
+    # 3. 구현
     
-    **3. 인접한 부분리스트끼리 정렬하여 합친다. (Conqure : 정복)**
+    ```java
     
-    # 3. Java 코드로 구현
+    public static void main(String[] args) {
+    
+            int[] arr = {5, 2, 3, 1, 7, 4, 6, 9};
+    
+            System.out.println(Arrays.toString(arr));
+    				// [5, 2, 3, 1, 7, 4, 6, 9]
+            System.out.println(Arrays.toString(merge(arr)));
+    				// [1, 2, 3, 4, 5, 6, 7, 9]
+    }
+    
+    public static int[] merge(int arr[]) {
+            if (arr.length<2){
+                return arr;
+            }
+            int mid = (arr.length) / 2;
+    
+            int[] left_arr = merge(Arrays.copyOfRange(arr, 0, mid));
+            int[] right_arr = merge(Arrays.copyOfRange(arr, mid, arr.length));
+    
+            int[] SORTED = new int[arr.length];
+            int left_p=0, right_p=0, idx=0;
+    
+            while(left_p < left_arr.length && right_p <right_arr.length){
+                if(left_arr[left_p] < right_arr[right_p]){
+                    SORTED[idx++] = left_arr[left_p++];
+                }else{
+                    SORTED[idx++] = right_arr[right_p++];
+                }
+            }
+    
+            while(left_p < left_arr.length){
+                SORTED[idx++] = left_arr[left_p++];
+            }
+    
+            while (right_p < right_arr.length){
+                SORTED[idx++] = right_arr[right_p++];
+            }
+    
+            return SORTED;
+        }
+    ```
     
     # 4. 알고리즘 분석
     
@@ -32,7 +72,7 @@
     
     1. 항상 두 부분리스트로 쪼개어 들어가기 때문에 최악의 경우에도 **O(NlogN)** 으로 유지가 된다.
     
-    2. 안정정렬
+    2. 안정정렬 
     
     **[단점]**
     
