91b00b53f7a0cd90d3a2d85e9f90ae3e8449c51d
januarry22 <tls2330@naver.com>
2022-07-12 16:47:22 +0900

    binary search tree

--- codingTest/src/code/test/datastructure/BinaryTree.java
+++ codingTest/src/code/test/datastructure/BinaryTree.java
@@ -1,11 +1,12 @@
 package code.test.datastructure;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
- * JAVA - 트리 구현
+ * JAVA - 이진  트리 구현
  * */
 public class BinaryTree {
-
-    Node root;
 
     public class Node {
         int data;
@@ -18,48 +19,171 @@ public class BinaryTree {
 
     }
 
-    public void addTree(int[] arr) {
-        root = addTreeNode(arr, 0, arr.length - 1);
-    }
+    Node root;
 
-    public Node addTreeNode(int[] tree, int start, int end) {
-
-        if (start > end) {
-            return null;
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (root == null) {
+            root = newNode;
+            return;
         }
 
-        int mid = (start + end) / 2;
-
-        Node node = new Node(tree[mid]);
-        node.left = addTreeNode(tree, start, mid - 1);
-        node.right = addTreeNode(tree, mid + 1, end);
-
-        return node;
+        Node currentNode= root;
+        while (true) {
+            // root 보다 data 가 작을 경우
+            if (data < currentNode.data) {
+                // root의 left 가 빈값이 아닐때
+                if (currentNode.left != null) {
+                    // 노드를 가리키는 위치를 left 자리로 옮김
+                    currentNode = currentNode.left;
+                } else {
+                    // null 이면 data 를 left에 넣음
+                    currentNode.left = newNode;
+                    break;
+                }
+            } else {
+                // root 보다 data 가 클 경우
+                // right 노드가 비어 있지 않을 경
+                if (currentNode.right != null) {
+                    currentNode = currentNode.right;
+                } else {
+                    // 비어 있으면 right 에 넣음
+                    currentNode.right = newNode;
+                    break;
+                }
+            }
+        }
+           System.out.println(newNode.data);
     }
 
-    public void searchTree(Node node, int find) {
 
-        if (find < node.data) {
-            System.out.println("방문 노드 : " + node.data);
-            searchTree(node.left, find);
-        } else if (find > node.data) {
-            System.out.println("방문 노드 : " + node.data);
-            searchTree(node.right, find);
-        } else {
-            System.out.println("Find");
+    public boolean search(int data) {
+        Node currentNode = root;
+
+        while (true) {
+            if (currentNode.data == data) {
+                return true;
+            } else if (data < currentNode.data) {
+                currentNode = currentNode.left;
+            } else {
+                currentNode = currentNode.right;
+            }
         }
     }
 
+    public boolean pop(int data) {
+        Node removeNode = root;
+        Node removeParent = null;
+
+        // root 가 삭제할 data 가 아니면
+        while (removeNode.data != data) {
+            // 현재 노드의 부모 노드로
+            removeParent = removeNode;
+
+            // 삭제할 값이 현재 값 보다 작으면
+            if (removeNode.data > data) {
+                // 현재 노드의 왼쪽을 가리킴
+                removeNode = removeNode.left;
+            } else {
+                removeNode = removeNode.right;
+            }
+        }
+        // leaf 노드 (단말 노드) 인경우
+        if (removeNode == null) {
+            return false;
+        }
+
+        /* 자식 노드가 없을 때 */
+        if (removeNode.left == null && removeNode.right == null) {
+            // root 이면 null
+            if (removeNode == root) {
+                root = null;
+            } else if (removeNode == removeParent.right) {
+                removeParent.right = null;
+            } else {
+                removeParent.left = null;
+            }
+        }
+
+        /* right 노드만 존재 할 경우 */
+        else if (removeNode.left == null) {
+            if (removeNode == root) {
+                root = removeNode.right;
+            } else if (removeNode == removeParent.right) {
+                removeParent.right = removeNode.right;
+            } else {
+                removeParent.left = removeNode.left;
+            }
+        }
+
+        /* left 노드만 존재 할 경우 */
+        else if (removeNode.right == null) {
+            if (removeNode == root) {
+                root = removeNode.left;
+            } else if (removeNode == removeParent.right) {
+                removeParent = removeNode.right;
+            } else {
+                removeParent = removeNode.left;
+            }
+        }
+
+        /* 두개의 자식 노드가 존재하는 경우 */
+
+        else {
+            Node parentReplace = removeNode;
+
+            Node replaceNode = parentReplace.right;
+
+            while (replaceNode.left != null) {
+                parentReplace = replaceNode;
+                replaceNode = replaceNode.left;
+            }
+
+            if (replaceNode != removeNode.right) {
+                parentReplace.left = replaceNode.right;
+                replaceNode.right = removeNode.right;
+            }
+
+            if (removeNode == root) {
+                root = removeNode;
+            } else if (removeNode == removeParent.right) {
+                removeParent.right = replaceNode;
+            } else {
+                removeParent.left = replaceNode;
+            }
+
+            replaceNode.left = removeNode.left;
+
+        }
+        return true;
+    }
+    /**
+     * 전위 순회
+     */
+    public void preorderTree(Node root, int depth) {
+        if (root != null) {
+            for (int i = 0; i < depth; i++) {
+                System.out.print("ㄴ");
+            }
+            System.out.println(root.data);
+            preorderTree(root.left, depth + 1);
+            preorderTree(root.right, depth + 1);
+        }
+    }
 
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
         int[] a = new int[10];
-        for (int i = 0; i < 10; i++) {
-            a[i] = i;
-        }
-        tree.addTree(a);
-        tree.searchTree(tree.root, 9);
 
+        tree.push(5);
+        tree.push(8);
+        tree.push(7);
+        tree.push(10);
+        tree.push(9);
+        tree.push(11);
+
+        tree.pop(10);
+        tree.preorderTree(tree.root, 0);
     }
 
 }
