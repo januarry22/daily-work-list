41b491cb8572dce5141a39225f2372559ecafcf1
januarry22 <tls2330@naver.com>
2022-08-14 18:24:40 +0900

    풀이

---
+++ codingTest/src/code/test/programmers/lessons87389.java
@@ -0,0 +1,21 @@
+package code.test.programmers;
+
+import java.util.Stack;
+
+/*
+ * [프로그래머스]
+ * */
+public class lessons87389 {
+
+    public static void main(String[] args) {
+       int n = 111100;
+
+       for (int i=3; i<=n; i++){
+           if(n%i==1){
+               System.out.println(i);
+               break;
+           }
+       }
+    }
+
+}
