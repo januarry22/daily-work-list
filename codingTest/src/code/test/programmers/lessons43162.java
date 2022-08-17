5d6912a87f20f7774b11f323c347321c23c28f03
januarry22 <tls2330@naver.com>
2022-08-17 19:29:04 +0900

    풀이

---
+++ codingTest/src/code/test/programmers/lessons43162.java
@@ -0,0 +1,68 @@
+package code.test.programmers;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * [프로그래머스] - 네트워크
+ * */
+public class lessons43162 {
+
+    static int visit[];
+    static int[][] computers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+
+    public static void main(String[] args) {
+        int answer = 0;
+        int n = 3;
+        visit = new int[n];
+
+//        for (int i = 0; i < n; i++) {
+//            if (visit[i] == 0) {
+//                answer++;
+//                bfs(n, i, computers);
+//            }
+//        }
+
+        for (int i = 0; i < n; i++) {
+            if (visit[i] == 0) {
+                answer++;
+                dfs(i);
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void bfs(int n, int node, int[][] computers) {
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        queue.add(node);
+
+        visit[node] = 1;
+
+        while (!queue.isEmpty()) {
+            int index = queue.poll();
+
+            for (int i = 0; i < n; i++) {
+                if (computers[index][i] == 1 && visit[i] == 0) {
+                    queue.add(i);
+                    visit[i] = 1;
+                }
+            }
+
+        }
+    }
+
+
+    public static void dfs( int index) {
+        visit[index] = 1;
+
+        for(int node : computers[index]){
+            if(visit[node]==0){
+                dfs(node);
+            }
+        }
+    }
+
+}
