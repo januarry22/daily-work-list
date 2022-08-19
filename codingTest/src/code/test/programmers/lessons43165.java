282e60e54556f9a16fdbc36c4fe4d75cb2cd7d1f
januarry22 <tls2330@naver.com>
2022-08-19 18:27:37 +0900

    dfs 풀이중

---
+++ codingTest/src/code/test/programmers/lessons43165.java
@@ -0,0 +1,24 @@
+package code.test.programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/*
+ * [프로그래머스] - 타겟 넘버
+ * */
+public class lessons43165 {
+    public static boolean[] visit;
+    public static int[] numbers = {1, 1, 1, 1, 1};
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void dfs(int depth, String now, String path, String[][] tickets) {
+
+    }
+
+
+}
