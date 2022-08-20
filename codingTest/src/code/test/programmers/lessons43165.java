441470bf41131f7a25128b9e573df7436573035b
januarry22 <tls2330@naver.com>
2022-08-20 19:03:22 +0900

    dfs 풀이중

--- codingTest/src/code/test/programmers/lessons43165.java
+++ codingTest/src/code/test/programmers/lessons43165.java
@@ -17,7 +17,18 @@ public class lessons43165 {
 
 
     public static void dfs(int depth, String now, String path, String[][] tickets) {
+        if (depth == tickets.length) {
+            list.add(path);
+            return;
+        }
 
+        for (int i = 0; i < tickets.length; i++) {
+            if (!visit[i] && now.equals(tickets[i][0])) {
+                visit[i] = true;
+                dfs(depth + 1, tickets[i][1], path + " " + tickets[i][1], tickets);
+                visit[i] = false;
+            }
+        }
     }
 
 
