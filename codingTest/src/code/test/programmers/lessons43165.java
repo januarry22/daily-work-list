bb9d5176fdfec21cee7445a5dcfcdefd05e7f8d5
januarry22 <tls2330@naver.com>
2022-08-30 18:34:04 +0900

    dfs 풀이

--- codingTest/src/code/test/programmers/lessons43165.java
+++ codingTest/src/code/test/programmers/lessons43165.java
@@ -8,23 +8,24 @@ import java.util.Collections;
  * [프로그래머스] - 타겟 넘버
  * */
 public class lessons43165 {
-    public static boolean[] visit;
     public static int[] numbers = {1, 1, 1, 1, 1};
+    public static int target= 3;
 
     public static void main(String[] args) {
 
-        dfs(0, "ICN", "ICN", numbers);
+
+        System.out.println(dfs(numbers, target , 0 , 0));
 
     }
 
+    public static int dfs(int[] numbers, int target, int index, int num) {
+        if (index == numbers.length) {
+            return num==target ? 1 : 0;
 
-    public static void dfs(int depth, String now, String path, int[] tickets) {
-
-        if (depth == tickets.length) {
-            list.add(path);
-            return;
+        }else{
+            return dfs(numbers, target, index+1, num+numbers[index]) +
+                    dfs(numbers, target, index+1, num-numbers[index]);
         }
-
     }
 
 
