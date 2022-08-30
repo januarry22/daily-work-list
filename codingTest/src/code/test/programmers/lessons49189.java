0f94777353e564000711249230f5a4b268e826bb
januarry22 <tls2330@naver.com>
2022-08-30 18:43:57 +0900

    그래프 풀이

---
+++ codingTest/src/code/test/programmers/lessons49189.java
@@ -0,0 +1,25 @@
+package code.test.programmers;
+
+import java.util.Arrays;
+
+/*
+* [프로그래머스] - 가장 먼 노드
+ * */
+public class lessons49189 {
+
+    public static boolean[] visited;
+    public static int[][] vertex = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
+    public static int n = 6;
+
+
+    public static void main(String[] args){
+
+
+    }
+
+
+    public static void dfs(){
+
+    }
+
+}
