86b5bb57758d18c89bb65d3ab317c9ead7f04ab9
januarry22 <tls2330@naver.com>
2022-09-06 17:08:06 +0900

    풀이

---
+++ codingTest/src/code/test/programmers/lessons42627.java
@@ -0,0 +1,16 @@
+package code.test.programmers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * [프로그래머스] - 디스크 컨트롤러
+ * */
+public class lessons42627 {
+    public static int[][] jobs = {{0, 3},{1, 9},{2, 6}};
+    public static void main(String[] args) {
+
+        Queue queue = new LinkedList();
+    }
+
+}
