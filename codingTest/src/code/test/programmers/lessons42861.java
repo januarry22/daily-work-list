4168123884e15be37ec1a9092ec9ff5a783a27d4
januarry22 <tls2330@naver.com>
2022-09-18 18:11:45 +0900

    42861

---
+++ codingTest/src/code/test/programmers/lessons42861.java
@@ -0,0 +1,20 @@
+package code.test.programmers;
+
+/*
+ * [프로그래머스] - 섬 연결하기
+ * */
+public class lessons42861 {
+
+    public static void main(String[] args) {
+        int[][] costs = {{0, 1, 1}, {0, 2, 2}, {1, 2, 5}, {1, 3, 1}, {2, 3, 8}};
+        int n = 4;
+
+        System.out.println(solution(n, costs));
+    }
+
+    public static int solution(int n, int[][] costs) {
+
+        return 0;
+    }
+
+}
