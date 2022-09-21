ad892976fe6d1f0195ea780076121aea53bf3f8f
januarry22 <tls2330@naver.com>
2022-09-21 21:22:03 +0900

    84021

---
+++ codingTest/src/code/test/programmers/lessons84021.java
@@ -0,0 +1,27 @@
+package code.test.programmers;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * [프로그래머스] - 퍼즐 조각 채우기
+ * */
+public class lessons84021 {
+
+    static boolean[] vistied;
+
+    public static void main(String[] args) {
+        int[][] game_board = {{0, 0, 0}, {1, 1, 0}, {1, 1, 1}};
+        int[][] table = {{1, 1, 1}, {1, 0, 0}, {0, 0, 0}};
+
+        System.out.println(solution(game_board , table));
+    }
+
+    static int solution(int[][] game_board, int[][] table) {
+
+        return 0;
+
+    }
+
+}
