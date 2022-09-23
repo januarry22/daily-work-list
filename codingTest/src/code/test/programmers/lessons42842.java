1430fc332be5d7b68bb5ae3db546ff1f1cf49672
januarry22 <tls2330@naver.com>
2022-09-23 19:01:46 +0900

    42842

---
+++ codingTest/src/code/test/programmers/lessons42842.java
@@ -0,0 +1,17 @@
+package code.test.programmers;
+
+public class lessons42842 {
+    public static void main(String[] args) {
+
+        int brown = 10;
+        int yellow = 2;
+    }
+
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+
+
+
+        return null;
+    }
+}
