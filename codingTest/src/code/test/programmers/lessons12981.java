fb306f9d60d3a7e2ff07f11c95e1706ef8340190
januarry22 <tls2330@naver.com>
2022-11-04 15:59:00 +0900

    12981 풀이

--- codingTest/src/code/test/programmers/lessons12981.java
+++ codingTest/src/code/test/programmers/lessons12981.java
@@ -1,5 +1,9 @@
 package code.test.programmers;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * [프로그래머스]
  * */
@@ -9,7 +13,7 @@ public class lessons12981 {
         String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
         int n = 3;
 
-        System.out.println(solution(words, n));
+        System.out.println(Arrays.toString(solution(words, n)));
     }
 
     public static int[] solution(String[] words, int n) {
@@ -17,16 +21,34 @@ public class lessons12981 {
 
         // 첫글자, 끝글자 동일
         // 동일 단어 금지
-        for (int i = 0; i < words.length; i++) {
-            char a = words[i].charAt(words[i].length() - 1);
-            for (int j = i + 1; j < words.length; j++) {
-                char b = words[j].charAt(0);
-                System.out.println(a+""+b);
-                if (a != b) {
+
+        int gameCount = 1;
+        int peopleCount = 0;
+        String prev = "";
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < words.length; i++){
+            String now = words[i];
+            peopleCount++;
+
+            if(i > 0){
+                //끝말 확인, 이미 존재하는 단어인지 확인
+                char prevChar = prev.charAt(prev.length()-1);
+                char nowChar = now.charAt(0);
+                if(prevChar != nowChar || map.containsKey(now)){
+                    answer[0] = peopleCount;
+                    answer[1] = gameCount;
+                    break;
                 }
-                if (words[i] == words[j]) {
-                    System.out.println((j + 1) / n);
-                }
+
+            }
+
+            map.put(now, 0);
+            prev = now;
+
+            if(peopleCount == n){
+                peopleCount = 0;
+                gameCount ++;
             }
         }
 
